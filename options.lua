1c2b17185c2d2704c7d5b8790e451883dc6b1867
babecka <babecka@outlook.com>
2024-02-12 14:33:58 +0100

    formatting

--- options.lua
+++ options.lua
@@ -10,7 +10,7 @@ SlashCmdList['MAGNIFY'] = function(msg)
 	if not args[1] then
 		DEFAULT_CHAT_FRAME:AddMessage('|cffffdc7aMagnify |cffffffffoptions')
 		DEFAULT_CHAT_FRAME:AddMessage(' ')
-        DEFAULT_CHAT_FRAME:AddMessage('|cffcecece/magnify |cffffffffarrow flash - toggle player indicator arrow flash')
+      DEFAULT_CHAT_FRAME:AddMessage('|cffcecece/magnify |cffffffffarrow flash - toggle player indicator arrow flash')
 		DEFAULT_CHAT_FRAME:AddMessage('|cffcecece/magnify |cffffffffarrow scale [number] - set player arrow scale')
 		DEFAULT_CHAT_FRAME:AddMessage('|cffcecece/magnify |cffffffffzoom reset - toggle whether to remember zoom level upon closing the map')
 		DEFAULT_CHAT_FRAME:AddMessage('|cffcecece/magnify |cffffffffzoom max [number] - set max world map zoom')
@@ -19,14 +19,14 @@ SlashCmdList['MAGNIFY'] = function(msg)
 			Magnify_Settings['arrow_flash'] = not Magnify_Settings['arrow_flash']
 
 			DEFAULT_CHAT_FRAME:AddMessage('|cffffdc7aMagnify|cffffffff: World map player arrow flash ' .. (Magnify_Settings['arrow_flash'] and '|cff4dd23denabled' or '|cffce2323disabled') .. '|cffffffff.')
-        elseif args[2] == 'scale' then
+      elseif args[2] == 'scale' then
             if args[3] then
-                Magnify_Settings['arrow_scale'] = tonumber(args[3])
+               Magnify_Settings['arrow_scale'] = tonumber(args[3])
 
-                DEFAULT_CHAT_FRAME:AddMessage('|cffffdc7aMagnify|cffffffff: World map player arrow scale set to |cff3083dd' .. args[3] .. '|cffffffff. This change will be applied after you reload your interface.')
+               DEFAULT_CHAT_FRAME:AddMessage('|cffffdc7aMagnify|cffffffff: World map player arrow scale set to |cff3083dd' .. args[3] .. '|cffffffff. This change will be applied after you reload your interface.')
             end
-        end
-    elseif args[1] == 'zoom' then
+      end
+   elseif args[1] == 'zoom' then
 		if args[2] == 'reset' then
 			Magnify_Settings['zoom_reset'] = not Magnify_Settings['zoom_reset']
 
